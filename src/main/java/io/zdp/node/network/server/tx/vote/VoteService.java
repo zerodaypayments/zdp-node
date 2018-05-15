95464b28a3f309347480aaafaa070b8092eb53b1
Sergey Nechaev <sergey.nechaev@equifax.com>
2018-05-15 17:28:30 +1000

    decentralization WIP

--- src/main/java/io/zdp/node/network/server/tx/vote/VoteService.java
+++ src/main/java/io/zdp/node/network/server/tx/vote/VoteService.java
@@ -5,10 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import io.zdp.api.model.v1.VoteRequest;
+import io.zdp.api.model.v1.VoteResponse;
+import io.zdp.api.model.v1.VoteResponse.Status;
 import io.zdp.crypto.Signing;
 import io.zdp.node.domain.Account;
 import io.zdp.node.network.server.tx.AccountTxCache;
-import io.zdp.node.network.server.tx.vote.VoteResponse.Status;
 import io.zdp.node.network.topology.NetworkNodeService;
 import io.zdp.node.service.AccountService;
 import io.zdp.node.service.NodeConfigurationService;
@@ -36,33 +38,39 @@ public class VoteService {
 
 		// If in 'current transactions' cache on any of the nodes -> stop
 		if (accountsInProgressCache.inProgress(req.getAccountUuidAsString())) {
-			return resp(req, Status.REJECTED_ACCOUNT_IN_PROGRESS);
+			return resp(null, req, Status.REJECTED_ACCOUNT_IN_PROGRESS);
 		}
 
 		// Load account from local storage
 		Account account = this.accountService.findByUuid(req.getAccountUuid());
 		if (account == null) {
-			return resp(req, Status.APPROVED_NO_ACCOUNT_ON_FILE);
+			return resp(null, req, Status.APPROVED_NO_ACCOUNT_ON_FILE);
 		}
 
 		// Start transaction
 		accountsInProgressCache.add(req.getAccountUuidAsString());
 
-		VoteResponse resp = resp(req, Status.APPROVED);
-
-		resp.setAccount(account);
+		VoteResponse resp = resp(account, req, Status.APPROVED);
 
 		return resp;
 
 	}
 
-	private VoteResponse resp(VoteRequest req, Status status) {
+	private VoteResponse resp(Account a, VoteRequest req, Status status) {
 
 		final VoteResponse resp = new VoteResponse();
 		resp.setStatus(status);
+		resp.setRequestUuid(req.getRequestUuid());
+
+		if (a != null) {
+
+			resp.setBalance(a.getBalance().toPlainString());
+			resp.setHeight(a.getHeight());
+			resp.setTransferChainHash(a.getTransferHash());
+		}
 
 		try {
-			byte[] signature = Signing.sign(nodeConfigService.getNode().getECPrivateKey(), req.getRequestUuid());
+			byte[] signature = Signing.sign(nodeConfigService.getNode().getECPrivateKey(), resp.toHashData());
 			resp.setSignedRequestUuid(signature);
 		} catch (Exception e) {
 			log.error("Error: ", e);
