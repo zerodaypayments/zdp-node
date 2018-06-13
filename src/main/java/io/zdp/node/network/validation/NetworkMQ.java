ae48dbb0a706aceae4aa6fcf4cbfc74a75b68e77
Sergey Nechaev <serg.nechaev@gmail.com>
2018-06-14 00:48:46 +1000

    moving to async mq

--- src/main/java/io/zdp/node/network/validation/NetworkMQ.java
+++ src/main/java/io/zdp/node/network/validation/NetworkMQ.java
@@ -1,5 +1,9 @@
 package io.zdp.node.network.validation;
 
+import java.util.Set;
+
+import javax.annotation.PostConstruct;
+
 import org.apache.commons.lang3.time.DateUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,21 +12,90 @@ import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 
 import io.zdp.model.network.NetworkNode;
+import io.zdp.model.network.NetworkTopologyListener;
 import io.zdp.model.network.NetworkTopologyService;
 import io.zdp.node.service.LocalNodeService;
 import io.zdp.node.service.validation.getAccounts.GetNodeAccountsRequest;
 import io.zdp.node.service.validation.getAccounts.GetNodeAccountsResponse;
 
 @Service
-public class NetworkMQ {
+public class NetworkMQ implements NetworkTopologyListener {
 
-	private final Logger log=LoggerFactory.getLogger(this.getClass());
-	
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+
 	@Autowired
 	private NetworkTopologyService networkTopologyService;
 
 	@Autowired
 	private LocalNodeService nodeConfigurationService;
+
+	static class NodeMQ {
+
+		private String serverUuid;
+
+		private String url;
+
+		public String getServerUuid() {
+			return serverUuid;
+		}
+
+		public void setServerUuid(String serverUuid) {
+			this.serverUuid = serverUuid;
+		}
+
+		public String getUrl() {
+			return url;
+		}
+
+		public void setUrl(String url) {
+			this.url = url;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((serverUuid == null) ? 0 : serverUuid.hashCode());
+			result = prime * result + ((url == null) ? 0 : url.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			NodeMQ other = (NodeMQ) obj;
+			if (serverUuid == null) {
+				if (other.serverUuid != null)
+					return false;
+			} else if (!serverUuid.equals(other.serverUuid))
+				return false;
+			if (url == null) {
+				if (other.url != null)
+					return false;
+			} else if (!url.equals(other.url))
+				return false;
+			return true;
+		}
+
+	}
+
+	private Set<>
+
+	@PostConstruct
+	public void init() {
+		onChange();
+		this.networkTopologyService.addChangeListener(this);
+	}
+
+	@Override
+	public void onChange() {
+		log.debug("Network topology changed, rebuild ");
+	}
 
 	/**
 	 * Send confirmed transfer back to the originating server
@@ -30,12 +103,12 @@ public class NetworkMQ {
 	public void send(final String serverUuid, final GetNodeAccountsResponse c) {
 
 		log.debug("Send [" + c + "] to [" + serverUuid + "]");
-		
+
 		final NetworkNode originatingNode = networkTopologyService.getNodeByUuid(serverUuid);
-		
+
 		final String brokerURL = "tcp://" + originatingNode.getAmqHostname() + ":" + originatingNode.getAmqPort();
 		log.debug("brokerURL: " + brokerURL);
-		
+
 		// Cache AMQ objects
 		org.apache.activemq.ActiveMQConnectionFactory amqcf = new org.apache.activemq.ActiveMQConnectionFactory();
 		amqcf.setBrokerURL(brokerURL);
@@ -46,17 +119,17 @@ public class NetworkMQ {
 		pcf.setMaxConnections(1024);
 		pcf.setTimeBetweenExpirationCheckMillis(DateUtils.MILLIS_PER_SECOND);
 		pcf.setUseAnonymousProducers(true);
-		
+
 		JmsTemplate template = new JmsTemplate(pcf);
 		template.setDeliveryPersistent(false);
 
-//S		template.convertAndSend(TransferConfirmationGateway.QUEUE_TRANSFER_CONFIRMATION, c);
+		//S		template.convertAndSend(TransferConfirmationGateway.QUEUE_TRANSFER_CONFIRMATION, c);
 
 	}
 
 	public void broadcastToValidationNetwork(GetNodeAccountsRequest transferConfirmationRequest) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
---
+++ src/main/java/io/zdp/node/service/validation/settle/TransferSettlementRequest.java
@@ -0,0 +1,67 @@
+package io.zdp.node.service.validation.settle;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+import io.zdp.node.service.validation.model.NetworkBaseSignedObject;
+import io.zdp.node.storage.account.domain.Account;
+import io.zdp.node.storage.transfer.domain.CurrentTransfer;
+
+/**
+ * Prepare transfer response
+ */
+@SuppressWarnings("serial")
+public class TransferSettlementRequest extends NetworkBaseSignedObject {
+
+	private Account fromAccount;
+
+	private Account toAccount;
+
+	private CurrentTransfer currentTransfer;
+
+	private byte[] transactionUuid;
+
+	public TransferSettlementRequest() {
+		super();
+	}
+
+	public Account getFromAccount() {
+		return fromAccount;
+	}
+
+	public void setFromAccount(Account fromAccount) {
+		this.fromAccount = fromAccount;
+	}
+
+	public Account getToAccount() {
+		return toAccount;
+	}
+
+	public void setToAccount(Account toAccount) {
+		this.toAccount = toAccount;
+	}
+
+	public CurrentTransfer getCurrentTransfer() {
+		return currentTransfer;
+	}
+
+	public void setCurrentTransfer(CurrentTransfer currentTransfer) {
+		this.currentTransfer = currentTransfer;
+	}
+
+	public byte[] getTransactionUuid() {
+		return transactionUuid;
+	}
+
+	public void setTransactionUuid(byte[] transactionUuid) {
+		this.transactionUuid = transactionUuid;
+	}
+
+	@Override
+	public String toString() {
+		return "TransferSettlementRequest [fromAccount=" + fromAccount + ", toAccount=" + toAccount + ", currentTransfer=" + currentTransfer + ", transactionUuid=" + Arrays.toString(transactionUuid) + "]";
+	}
+
+}
